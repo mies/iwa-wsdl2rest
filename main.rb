409d05192b584970be27d8d738054f8b448d1644
Micha Hernandez van Leuffen <mies@mies.local>
2009-09-27 22:39:21 +0200

    initial commit

---
+++ main.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+require 'soap/wsdlDriver'
+
+$URL = 'http://www.webservicex.net/geoipservice.asmx?wsdl'
+
+wsdl = $URL
+driver = SOAP::WSDLDriverFactory.new(wsdl).create_rpc_driver
+
+get '/' do
+    erb :index
+end
+
+post '/location' do
+    request = driver.getGeoIP('IPAddress' => params[:ip])
+    result = request.getGeoIPResult()
+    erb :location, :locals => {:ip => params[:ip], :country => result["CountryName"]}
+end
+
+get '/api/location.:format' do
+    request = driver.getGeoIP('IPAddress' => params[:ip])
+    result = request.getGeoIPResult()
+    location = result["CountryName"]
+    case params[:format]
+    when 'xml'
+        content_type :xml
+        builder do |xml|
+            xml.instruct!
+            xml.countryname location
+        end
+    when 'json'
+        content_type('application/json')
+        {:CountryName => location}.to_json
+    else
+        content_type :json
+        {:CountryName => location}.to_json
+    end
+end
