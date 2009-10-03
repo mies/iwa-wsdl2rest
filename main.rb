0a1f007c39e49d618ded7082a1c18f31254356af
Micha Hernandez van Leuffen <mies@mies.local>
2009-10-03 11:11:22 +0200

    trying out ip getter

--- main.rb
+++ main.rb
@@ -10,8 +10,15 @@ driver = SOAP::WSDLDriverFactory.new(wsdl).create_rpc_driver
 
 get '/' do
     #ip = request.env['HTTP_X_REAL_IP']
-    ipRange = request.env['REMOTE_ADDR']
-    ip = ipRange[0]
+    #ipRange = request.env['REMOTE_ADDR']
+    #ip = @env['HTTP_CLIENT_IP']
+    #puts ip
+    #ip2 = @env['HTTP_X_FORWARDED_FOR']
+    #puts ip2
+    ipArray = request.env['REMOTE_ADDR'].split(/,/)
+    #ip = "192.434.54.34, 34.55.53.22"
+    
+    ip = ipArray[0]
     erb :index, :locals => {:ip => ip}
 end
 
